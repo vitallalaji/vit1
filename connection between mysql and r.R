ab64b42e8bae3a894bd12ef40cdce03fd1617711
vitallalaji <45336918+vitallalaji@users.noreply.github.com>
2019-01-31 18:20:35 +0530

    Update connection between mysql and r.R

--- connection between mysql and r.R
+++ connection between mysql and r.R
@@ -7,7 +7,7 @@ localuserpassword <- "1234"
 # The connection method below uses a password stored in a variable.  
 # To use this set localuserpassword="The password of newspaper_search_results_user" 
 
-storiesDb <- dbConnect(RMariaDB::MariaDB(), user='vittu', password=localuserpassword, dbname='vital', host='localhost')
+storiesDb <- dbConnect(RMariaDB::MariaDB(), user='xxxx', password=localuserpassword, dbname='xxxx', host='localhost')
 dbListTables(storiesDb)
 x<-"select* from nat;"
 data<-dbGetQuery(storiesDb,x)
